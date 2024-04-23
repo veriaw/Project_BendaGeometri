b67eb6aa047d5b7b53421fa84dce7912c6f60c5a
adity <adity@Royan>
2024-04-23 14:27:02 +0700

    add lingkaran

--- src/geometri2D/Lingkaran.java
+++ src/geometri2D/Lingkaran.java
@@ -4,10 +4,31 @@
  */
 package geometri2D;
 
+import bendaGeometri.BendaGeometri;
+import bendaGeometri.Geometri2D;
+
 /**
  *
  * @author Lenovo
  */
-public class Lingkaran {
+public class Lingkaran extends BendaGeometri implements Geometri2D{
+    protected double jari_jari;
+    public Lingkaran(double jari_jari){
+        this.jari_jari=jari_jari;
+    }
+
+    public double getJari_jari() {
+        return jari_jari;
+    }
+
+    public void setJari_jari(double jari_jari) {
+        this.jari_jari = jari_jari;
+    }
+
+    
+    @Override
+    public double luas() {
+        return Math.PI*jari_jari*jari_jari;
+    }
     
 }
