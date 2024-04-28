447fd25e20d68158fc3acc76e3b7b2b1369a7d69
adity <adity@Royan>
2024-04-28 15:42:14 +0700

    add juring tembereng

---
+++ src/bangunDatar/JuringLingkaran.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bangunDatar;
+
+import bendaGeometri.BendaGeometri;
+import bendaGeometri.GeometriDatar;
+
+/**
+ *
+ * @author adity
+ */
+public class JuringLingkaran extends BendaGeometri implements GeometriDatar {
+
+    protected double jari_jari, derajat_sudut;
+    public JuringLingkaran(double jari_jari,double derajat_sudut){
+        this.jari_jari=jari_jari;
+        this.derajat_sudut=derajat_sudut;
+    }
+
+    public double getJari_jari() {
+        return jari_jari;
+    }
+
+    public void setJari_jari(double jari_jari) {
+        this.jari_jari = jari_jari;
+    }
+
+    public double getDerajat_sudut() {
+        return derajat_sudut;
+    }
+
+    public void setDerajat_sudut(double derajat_sudut) {
+        this.derajat_sudut = derajat_sudut;
+    }
+
+    @Override
+    public double luas() {
+        return derajat_sudut/360 * Math.PI*Math.pow(jari_jari,2);
+    }
+    
+}
